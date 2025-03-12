10e1dccae22036e4e9bd44f006281d76b2fb609e
gabrisp-utad <gabriel.sanchez@live.u-tad.com>
2025-03-12 09:35:02 +0100

    dbConnect

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const dbConnect = require('./utils/mongo')
+
+require ('dotenv').config()
+
+
+const app = express();
+
+
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT || 3000
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+dbConnect()
